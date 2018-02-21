fc2dadca1a2989c7635859709939bae9b13b1c7a
Peter <ptk726@gmail.com>
2018-02-21 04:48:55 -0500

    updated

--- public/js/app.js
+++ public/js/app.js
@@ -270,35 +270,6 @@ app.controller('MainController', ['$http', function ($http) {
 
 
 
-
-
-
-    
-    // -----google map ---------------------
-
-
-
-    // To use this code on your website, get a free key from Google.
-    // Read more at: https://www.w3schools.com/graphics/google_maps_basic.asp
-
-    // function myMap() {
-    //     myCenter = new google.maps.LatLng(41.878114, -87.629798);
-    //     var mapOptions = {
-    //         center: myCenter,
-    //         zoom: 12, scrollwheel: false, draggable: false,
-    //         mapTypeId: google.maps.MapTypeId.ROADMAP
-    //     };
-    //     var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
-
-    //     var marker = new google.maps.Marker({
-    //         position: myCenter,
-    //     });
-    //     marker.setMap(map);
-    // }
-
-    
-
-
 // Travel Info - Amadeus travel request ----------------
     // this.destination = {};
     // this.departureDate = [];
